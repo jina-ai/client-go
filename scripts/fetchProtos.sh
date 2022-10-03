0c372c4e90b9545507b3a941f2aa55a619886663
Deepankar Mahapatro <deepankar.mahapatro@jina.ai>
2022-10-03 11:11:25 +0530

    ci: version update

--- scripts/fetchProtos.sh
+++ scripts/fetchProtos.sh
@@ -13,9 +13,17 @@ if [ -z "$JINA_VERSION" ]; then
     exit 1
 fi
 
+if [[ $JINA_VERSION != "v*" ]]; then
+    JINA_VERSION="v$JINA_VERSION"
+fi
+
 if [ -z "$DOCARRAY_VERSION" ]; then
     echo "Please provide a Docarray version as the second argument."
     exit 1
+fi
+
+if [[ $DOCARRAY_VERSION != "v*" ]]; then
+    DOCARRAY_VERSION="v$DOCARRAY_VERSION"
 fi
 
 echo "Fetching protos for Jina version $JINA_VERSION"
