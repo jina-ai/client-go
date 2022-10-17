c951aa78a622386e90434c37a255f8ff201798eb
Nikolas Pitsillos <npitsillos@gmail.com>
2022-10-17 14:33:18 +0100

    feat: jina & docarray multiple versions

--- scripts/protogen.sh
+++ scripts/protogen.sh
@@ -3,6 +3,8 @@
 set -e
 
 # This script is used to generate gRPC client stubs from the proto files.
+JINA_VERSION=$1
+DOCARRAY_VERSION=$2
 
 GO_MODULE="github.com/jina-ai/client-go"
 DOCARRAY_PROTO="docarray.proto"
@@ -13,21 +15,28 @@ JINA_PROTO="jina.proto"
 JINA_DIR="../jina"
 JINA_PACKAGE="$GO_MODULE/jina"
 
+mkdir $DOCARRAY_DIR/v$DOCARRAY_VERSION
+mkdir $DOCARRAY_DIR/v$JINA_VERSION
 
 cd protos
 grep -q '^option go_package = ' docarray.proto || sed -i '/package docarray;/aoption go_package = "'${DOCARRAY_PACKAGE}'";' docarray.proto
-protoc --go_out=${DOCARRAY_DIR} \
+protoc --go_out=${DOCARRAY_DIR}/v${DOCARRAY_VERSION} \
        --go_opt=paths=source_relative \
        --go_opt=M${DOCARRAY_PROTO}=${DOCARRAY_PACKAGE} \
-       --go-grpc_out=${DOCARRAY_DIR} \
+       --go-grpc_out=${DOCARRAY_DIR}/v${DOCARRAY_VERSION} \
        --go-grpc_opt=paths=source_relative \
        ${DOCARRAY_PROTO} 
 
+cp $DOCARRAY_DIR/json.go $DOCARRAY_DIR/v$DOCARRAY_VERSION
+
 grep -q '^option go_package = ' jina.proto || sed -i '/package jina;/aoption go_package = "'${JINA_PACKAGE}'";' jina.proto
-protoc --go_out=${JINA_DIR} \
+protoc --go_out=${JINA_DIR}/v${JINA_VERSION} \
        --go_opt=paths=source_relative \
        --go_opt=M${JINA_PROTO}=${JINA_PACKAGE} \
-       --go-grpc_out=${JINA_DIR} \
+       --go-grpc_out=${JINA_DIR}/v${JINA_VERSION} \
        --go-grpc_opt=paths=source_relative \
+       --experimental_allow_proto3_optional \
        ${JINA_PROTO} 
+
+cp $JINA_DIR/json.go $JINA_DIR/v$JINA_VERSION
 cd -
